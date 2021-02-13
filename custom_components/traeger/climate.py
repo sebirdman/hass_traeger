f5eaba2320a96dcb8eed2908f22f585289d5f9bc
Stephen Papierski <stephenpapierski@gmail.com>
2021-02-13 00:10:56 -0700

    Properties now return info from memory Per HA docs, "Properties should always only return information from memory and not do I/O (like network requests)."

--- custom_components/traeger/climate.py
+++ custom_components/traeger/climate.py
@@ -1,4 +1,4 @@
-"""Binary sensor platform for integration_blueprint."""
+"""Climate platform for Traeger grills"""
 
 import logging
 
@@ -20,6 +20,14 @@ from homeassistant.const import (
 from .const import (
     DEFAULT_NAME,
     DOMAIN,
+    GRILL_MODE_OFFLINE,
+    GRILL_MODE_COOL_DOWN,
+    GRILL_MODE_CUSTOM_COOK,
+    GRILL_MODE_MANUAL_COOK,
+    GRILL_MODE_PREHEATING,
+    GRILL_MODE_IGNITING,
+    GRILL_MODE_IDLE,
+    GRILL_MODE_SLEEPING,
 )
 
 from .entity import IntegrationBlueprintEntity
@@ -31,29 +39,40 @@ async def async_setup_entry(hass, entry, async_add_devices):
     client = hass.data[DOMAIN][entry.entry_id]
     grills = client.get_grills()
     for grill in grills:
-        async_add_devices(
-            [IntegrationBlueprintBinarySensor(client, grill["thingName"])]
-        )
+        async_add_devices([TraegerClimateEntity(client, grill["thingName"])])
 
 
-class IntegrationBlueprintBinarySensor(ClimateEntity, IntegrationBlueprintEntity):
-    """integration_blueprint binary_sensor class."""
-
-    def grill_update(self):
-        self.schedule_update_ha_state()
+class TraegerClimateEntity(ClimateEntity, IntegrationBlueprintEntity):
+    """Climate entity for Traeger grills"""
 
     def __init__(self, client, grill_id):
         self.grill_id = grill_id
         self.client = client
+        self.grill_details = None
+        self.grill_state = None
+        self.grill_units = None
+        self.grill_limits = None
+
+        # Tell the Traeger client to call grill_update() when it gets an update
         self.client.set_callback_for_grill(self.grill_id, self.grill_update)
 
+    def grill_update(self):
+        """This gets called when the grill has an update. Update state variable"""
+        self.grill_details = self.client.get_details_for_device(self.grill_id)
+        self.grill_state = self.client.get_state_for_device(self.grill_id)
+        self.grill_units = self.client.get_units_for_device(self.grill_id)
+        self.grill_limits = self.client.get_limits_for_device(self.grill_id)
+
+        # Tell HA we have an update
+        self.schedule_update_ha_state()
+
+    # Generic Properties
     @property
     def name(self):
-        """Return the name of the binary_sensor."""
-        state = self.client.get_details_for_device(self.grill_id)
-        if state is None:
+        """Return the name of the grill"""
+        if self.grill_details is None:
             return f"{self.grill_id}"
-        return state["friendlyName"]
+        return self.grill_details["friendlyName"]
 
     @property
     def unique_id(self):
@@ -63,68 +82,65 @@ class IntegrationBlueprintBinarySensor(ClimateEntity, IntegrationBlueprintEntity
     def icon(self):
         return "mdi:grill"
 
+    # Climate Properties
+    @property
+    def temperature_unit(self):
+        if self.grill_units == TEMP_CELSIUS:
+            return TEMP_CELSIUS
+        else:
+            return TEMP_FAHRENHEIT
+
     @property
     def current_temperature(self):
-        state = self.client.get_state_for_device(self.grill_id)
-        if state is None:
+        if self.grill_state is None:
             return 0
-        return state["grill"]
+        return self.grill_state["grill"]
 
     @property
     def target_temperature(self):
-        state = self.client.get_state_for_device(self.grill_id)
-        if state is None:
+        if self.grill_state is None:
             return 0
-        return state["set"]
+        return self.grill_state["set"]
+
+    @property
+    def max_temp(self):
+        if self.grill_limits is None:
+            return self.min_temp
+        return self.grill_limits["max_grill_temp"]
 
     @property
     def min_temp(self):
         # this was the min the traeger app would let me set
-        if self.client.get_units_for_device(self.grill_id) == TEMP_CELSIUS:
+        if self.grill_units == TEMP_CELSIUS:
             return 75
         else:
             return 165
-
-    @property
-    def max_temp(self):
-        limits = self.client.get_limits_for_device(self.grill_id)
-        if limits is None:
-            return self.min_temp
-        return limits["max_grill_temp"]
-
-    @property
-    def supported_features(self):
-        """Return the list of supported features for the grill"""
-        return SUPPORT_TARGET_TEMPERATURE
-
-    @property
-    def temperature_unit(self):
-        if self.client.get_units_for_device(self.grill_id) == TEMP_CELSIUS:
-            return TEMP_CELSIUS
-        else:
-            return TEMP_FAHRENHEIT
 
     @property
     def hvac_mode(self):
         """Return hvac operation ie. heat, cool mode.
         Need to be one of HVAC_MODE_*.
         """
-        state = self.client.get_state_for_device(self.grill_id)
-        if state is None:
+        if self.grill_state is None:
             return HVAC_MODE_OFF
-        elif state["system_status"] == 8:  # Cool Down
+
+        state = self.grill_state["system_status"]
+
+        if state == GRILL_MODE_COOL_DOWN:  # Cool Down
             return HVAC_MODE_COOL
-        elif state["system_status"] == 7:  # Custom Cook
+        elif state == GRILL_MODE_CUSTOM_COOK:  # Custom Cook
             return HVAC_MODE_HEAT
-        elif state["system_status"] == 6:  # Manual Cook
+        elif state == GRILL_MODE_MANUAL_COOK:  # Manual Cook
             return HVAC_MODE_HEAT
-        elif state["system_status"] == 5:  # Preheating
+        elif state == GRILL_MODE_PREHEATING:  # Preheating
             return HVAC_MODE_HEAT
-        elif state["system_status"] == 4:  # Igniting
+        elif state == GRILL_MODE_IGNITING:  # Igniting
             return HVAC_MODE_HEAT
-        elif state["system_status"] == 3:  # Idle (Power switch on, screen on)
+        elif state == GRILL_MODE_IDLE:  # Idle (Power switch on, screen on)
             return HVAC_MODE_OFF
-        elif state["system_status"] == 2:  # Sleeping (Power switch on, screen off)
+        elif state == GRILL_MODE_SLEEPING:  # Sleeping (Power switch on, screen off)
+            return HVAC_MODE_OFF
+        elif state == GRILL_MODE_OFFLINE:  # Offline
             return HVAC_MODE_OFF
         else:
             return HVAC_MODE_OFF
@@ -136,6 +152,12 @@ class IntegrationBlueprintBinarySensor(ClimateEntity, IntegrationBlueprintEntity
         """
         return (HVAC_MODE_HEAT, HVAC_MODE_OFF, HVAC_MODE_COOL)
 
+    @property
+    def supported_features(self):
+        """Return the list of supported features for the grill"""
+        return SUPPORT_TARGET_TEMPERATURE
+
+    # Climate Methods
     async def async_set_temperature(self, **kwargs):
         """Set new target temperature."""
         temperature = kwargs.get(ATTR_TEMPERATURE)
