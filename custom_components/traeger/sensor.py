2418dab659b3765b5f79df2c28fb470302da972d
njobrien1006 <59174199+njobrien1006@users.noreply.github.com>
2021-09-20 08:27:26 -0500

    Add epoch timer read-back.
    Added the timer class and updated naming.

--- custom_components/traeger/sensor.py
+++ custom_components/traeger/sensor.py
@@ -18,19 +18,19 @@ async def async_setup_entry(hass, entry, async_add_devices):
     grills = client.get_grills()
     for grill in grills:
         grill_id = grill["thingName"]
-        await client.update_state(grill_id)
+        #await client.update_state(grill_id) We already have this in the init.py
         state = client.get_state_for_device(grill_id)
         if state is None:
             return
-        for accessory in state["acc"]:
-            if accessory["type"] == "probe":
-                async_add_devices(
-                    [AccessoryTemperatureSensor(client, grill_id, accessory["uuid"])]
-                )
-
-        async_add_devices([ValueTemperature(client, grill["thingName"], "grill")])
+        state = client.get_features_for_device(grill_id)
+        if state is None:
+            return
+        if state["pellet_sensor_enabled"]:
+            async_add_devices([PelletSensor(client, grill["thingName"], "pellet_level")])
+        #async_add_devices([ValueTemperature(client, grill["thingName"], "grill")]) Duplicate of Climate Entity
         async_add_devices([ValueTemperature(client, grill["thingName"], "ambient")])
-        async_add_devices([PelletSensor(client, grill["thingName"], "pellet_level")])
+        async_add_devices([GrillTimer(client, grill["thingName"], "cook_timer_start")])
+        async_add_devices([GrillTimer(client, grill["thingName"], "cook_timer_end")])
 
 
 class ValueTemperature(IntegrationBlueprintEntity):
@@ -40,6 +40,7 @@ class ValueTemperature(IntegrationBlueprintEntity):
         self.grill_id = grill_id
         self.client = client
         self.value = value
+        self.grill_details = None
         self.grill_state = None
         self.grill_units = None
 
@@ -49,6 +50,7 @@ class ValueTemperature(IntegrationBlueprintEntity):
     def grill_update(self):
         self.grill_state = self.client.get_state_for_device(self.grill_id)
         self.grill_units = self.client.get_units_for_device(self.grill_id)
+        self.grill_details = self.client.get_details_for_device(self.grill_id)
 
         # Tell HA we have an update
         self.schedule_update_ha_state()
@@ -64,12 +66,15 @@ class ValueTemperature(IntegrationBlueprintEntity):
 
     @property
     def name(self):
-        """Return the name of the sensor."""
-        return f"{self.value.capitalize()} Temperature"
+        """Return the name of the grill"""
+        if self.grill_details is None:
+            return f"{self.grill_id}_{self.value}"              
+        name = self.grill_details["friendlyName"]
+        return f"{name} {self.value.capitalize()}"              
 
     @property
     def unique_id(self):
-        return f"{self.grill_id}-{self.value}"
+        return f"{self.grill_id}_{self.value}"                  
 
     @property
     def icon(self):
@@ -124,16 +129,15 @@ class AccessoryTemperatureSensor(IntegrationBlueprintEntity):
 
     @property
     def name(self):
-        """Return the name of the sensor."""
+        """Return the name of the grill"""
         if self.grill_details is None:
-            return f"{self.grill_id}-{self.sensor_id}"
-
+            return f"{self.grill_id}_{self.value}"              
         name = self.grill_details["friendlyName"]
-        return f"{name}-{self.sensor_id}"
+        return f"{name} {self.value.capitalize()}"              
 
     @property
     def unique_id(self):
-        return f"{self.grill_id}-{self.sensor_id}"
+        return f"{self.grill_id}_{self.value}"                                
 
     @property
     def icon(self):
@@ -158,6 +162,7 @@ class PelletSensor(IntegrationBlueprintEntity):
         self.grill_id = grill_id
         self.client = client
         self.value = value
+        self.grill_details = None
         self.grill_state = None
         self.grill_units = None
 
@@ -167,6 +172,7 @@ class PelletSensor(IntegrationBlueprintEntity):
     def grill_update(self):
         self.grill_state = self.client.get_state_for_device(self.grill_id)
         self.grill_units = self.client.get_units_for_device(self.grill_id)
+        self.grill_details = self.client.get_details_for_device(self.grill_id)
 
         # Tell HA we have an update
         self.schedule_update_ha_state()
@@ -182,12 +188,15 @@ class PelletSensor(IntegrationBlueprintEntity):
 
     @property
     def name(self):
-        """Return the name of the sensor."""
-        return f"{self.value.replace('_',' ').title()}"
+        """Return the name of the grill"""
+        if self.grill_details is None:
+            return f"{self.grill_id}_{self.value}"              
+        name = self.grill_details["friendlyName"]
+        return f"{name} {self.value.capitalize()}"              
 
     @property
     def unique_id(self):
-        return f"{self.grill_id}-{self.value}"
+        return f"{self.grill_id}_{self.value}"                  
 
     @property
     def icon(self):
@@ -203,3 +212,61 @@ class PelletSensor(IntegrationBlueprintEntity):
     @property
     def unit_of_measurement(self):
         return "%"
+    
+class GrillTimer(IntegrationBlueprintEntity):
+    """Traeger Timer class."""
+
+    def __init__(self, client, grill_id, value):
+        self.grill_id = grill_id
+        self.client = client
+        self.value = value
+        self.grill_details = None
+        self.grill_state = None
+        self.grill_units = None
+
+        # Tell the Traeger client to call grill_update() when it gets an update
+        self.client.set_callback_for_grill(self.grill_id, self.grill_update)
+
+    def grill_update(self):
+        self.grill_state = self.client.get_state_for_device(self.grill_id)
+        self.grill_units = self.client.get_units_for_device(self.grill_id)
+        self.grill_details = self.client.get_details_for_device(self.grill_id)
+
+        # Tell HA we have an update
+        self.schedule_update_ha_state()
+
+    # Generic Properties
+    # @property
+    # def available(self):
+    #    """Reports unavailable when the grill is powered off"""
+    #    if self.grill_state is None:
+    #        return False
+    #    else:
+    #        return True if self.grill_state["connected"] == True else False
+
+    @property
+    def name(self):
+        """Return the name of the grill"""
+        if self.grill_details is None:
+            return f"{self.grill_id}_{self.value}"              
+        name = self.grill_details["friendlyName"]
+        return f"{name} {self.value.capitalize()}"             
+
+    @property
+    def unique_id(self):
+        return f"{self.grill_id}_{self.value}"                 
+
+    @property
+    def icon(self):
+        return "mdi:timer"
+
+    # Sensor Properties
+    @property
+    def state(self):
+        if self.grill_state is None:
+            return 0
+        return self.grill_state[f"{self.value}"]
+
+    @property
+    def unit_of_measurement(self):
+        return "sec"
