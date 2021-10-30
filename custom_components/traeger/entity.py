343d6840b3876c21d76928cc38db0bbc216fde83
Stephen Bird <sebirdman@gmail.com>
2021-10-30 06:22:45 +0000

    Reduce code and make entities unavailable when unusable

--- custom_components/traeger/entity.py
+++ custom_components/traeger/entity.py
@@ -1,10 +1,34 @@
-"""BlueprintEntity class"""
+"""TraegerBaseEntity class"""
 from homeassistant.helpers.entity import Entity
 
 from .const import DOMAIN, NAME, VERSION, ATTRIBUTION
 
+class TraegerBaseEntity(Entity):
 
-class IntegrationBlueprintEntity(Entity):
+    def __init__(self, client, grill_id):
+        super().__init__()
+        self.grill_id = grill_id
+        self.client = client
+        self.grill_refresh_state()
+
+    def grill_refresh_state(self):
+        self.grill_state = self.client.get_state_for_device(self.grill_id)
+        self.grill_units = self.client.get_units_for_device(self.grill_id)
+        self.grill_details = self.client.get_details_for_device(self.grill_id)
+        self.grill_features = self.client.get_features_for_device(self.grill_id)
+        self.grill_settings = self.client.get_settings_for_device(self.grill_id)
+        self.grill_limits = self.client.get_limits_for_device(self.grill_id)
+        self.grill_cloudconnect = self.client.get_cloudconnect(self.grill_id)
+
+    def grill_register_callback(self):
+        # Tell the Traeger client to call grill_update() when it gets an update
+        self.client.set_callback_for_grill(self.grill_id, self.grill_update_internal)
+
+    def grill_update_internal(self):
+        self.grill_refresh_state()
+
+        # Tell HA we have an update
+        self.schedule_update_ha_state()
 
     @property
     def unique_id(self):
@@ -17,10 +41,8 @@ class IntegrationBlueprintEntity(Entity):
 
     @property
     def device_info(self):
-        settings = self.client.get_settings_for_device(self.grill_id)
-        details = self.client.get_details_for_device(self.grill_id)
 
-        if settings is None:
+        if self.grill_settings is None:
             return {
                 "identifiers": {(DOMAIN, self.grill_id)},
                 "name": NAME,
@@ -29,9 +51,9 @@ class IntegrationBlueprintEntity(Entity):
 
         return {
             "identifiers": {(DOMAIN, self.grill_id)},
-            "name": details["friendlyName"],
-            "model": settings["device_type_id"],
-            "sw_version": settings["fw_version"],
+            "name": self.grill_details["friendlyName"],
+            "model": self.grill_settings["device_type_id"],
+            "sw_version": self.grill_settings["fw_version"],
             "manufacturer": NAME
         }
 
--- custom_components/traeger/number.py
+++ custom_components/traeger/number.py
@@ -1,16 +1,11 @@
 """Number/Timer platform for Traeger."""
-import logging
-
 from homeassistant.components.number import NumberEntity
 
 from .const import (
-    DEFAULT_NAME,
     DOMAIN,
 )
 
-from .entity import IntegrationBlueprintEntity
-from .traeger import traeger
-
+from .entity import TraegerBaseEntity
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup Number/Timer platform."""
@@ -18,44 +13,28 @@ async def async_setup_entry(hass, entry, async_add_devices):
     grills = client.get_grills()
     for grill in grills:
         grill_id = grill["thingName"]
-        #await client.update_state(grill_id)
-        state = client.get_state_for_device(grill_id)
-        if state is None:
-            return
         async_add_devices([TraegerNumberEntity(client, grill["thingName"], "cook_timer")])
 
-class TraegerNumberEntity(NumberEntity, IntegrationBlueprintEntity):
+class TraegerNumberEntity(NumberEntity, TraegerBaseEntity):
     """Traeger Number/Timer Value class."""
 
     def __init__(self, client, grill_id, devname):
-        self.grill_id = grill_id
-        self.client = client
+        super().__init__(client, grill_id)
         self.devname = devname
-        self.grill_state = self.client.get_state_for_device(self.grill_id)
-        self.grill_details = self.client.get_details_for_device(self.grill_id)
-
-        # Tell the Traeger client to call grill_update() when it gets an update
-        self.client.set_callback_for_grill(self.grill_id, self.grill_update)
-
-    def grill_update(self):
-        self.grill_state = self.client.get_state_for_device(self.grill_id)
-        self.grill_details = self.client.get_details_for_device(self.grill_id)
-
-        # Tell HA we have an update
-        self.schedule_update_ha_state()
+        self.grill_register_callback()
 
     # Generic Properties
     @property
     def name(self):
         """Return the name of the grill"""
         if self.grill_details is None:
-            return f"{self.grill_id}_{self.devname}"              
+            return f"{self.grill_id}_{self.devname}"
         name = self.grill_details["friendlyName"]
-        return f"{name} {self.devname.capitalize()}"             
+        return f"{name} {self.devname.capitalize()}"
 
     @property
     def unique_id(self):
-        return f"{self.grill_id}_{self.devname}"                  
+        return f"{self.grill_id}_{self.devname}"
 
     @property
     def icon(self):
