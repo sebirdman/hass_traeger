b1c0f735442362087394b8fd23983d84330a5a5d
njobrien1006 <59174199+njobrien1006@users.noreply.github.com>
2021-12-21 14:55:14 -0600

    Update __init__.py

--- custom_components/traeger/__init__.py
+++ custom_components/traeger/__init__.py
@@ -65,6 +65,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     async def async_shutdown(event: Event):
         """Shut down the client."""
         await client.kill()
+        
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, async_shutdown)
     entry.add_update_listener(async_reload_entry)
     return True
